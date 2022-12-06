f4a9ffb33361443c1896086178ec28b86b013ad3
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2022-12-06 11:03:28 +0530

    msg91 configured

--- routes/auth.js
+++ routes/auth.js
@@ -1,66 +1,83 @@
-const router=require("express").Router();
-const crypto=require("crypto");
+const router = require("express").Router();
+const crypto = require("crypto");
+const http = require("http");
 require('dotenv').config()
 
-const twilio=require("twilio")("ACe81b3e9154a9f3dac04677603ddac4dd",
-	"4b193a95e1b3f4a430bc62c9a8598f06",{lazyLoading:true});
-
-router.post("/generate-otp/:phone",(req,res)=>{
-	try{
-		const phone =req.params.phone;
-		const otp=crypto.randomInt(100000,999999);
-		const maxTime=1000*60*2;
-		const expiresIn=Date.now()+maxTime;
-		const hash=createHashedString(`${phone}.${otp}.${expiresIn}`);
+router.post("/generate-otp/:phone", (req, res) => {
+	try {
+		const phone = req.params.phone;
+		const otp = crypto.randomInt(100000, 999999);
+		const maxTime = 1000 * 60 * 2;
+		const expiresIn = Date.now() + maxTime;
+		const hash = createHashedString(`${phone}.${otp}.${expiresIn}`);
 		sendOtp(phone, otp);
 		// for production just remove from res send and uncomment send sms
-		res.status(200).json({hash:`${hash}.${expiresIn}`});
-	}catch(e){
-        console.log(e)
-		res.status(400).json({msg:"Netwok Connection Error2"});
+		res.status(200).json({ hash: `${hash}.${expiresIn}` });
+	} catch (e) {
+		console.log(e)
+		res.status(400).json({ msg: "Netwok Connection Error2" });
 	}
-	
+
 })
 
 
-router.post("/verify-otp/:details",async (req,res)=>{
-	try{
-    console.log(req.params)
-		const details=req.params.details.split("&");
-		const phone=details[0];
-		const hash=details[1];
-		const otp=details[2];
+router.post("/verify-otp/:details", async (req, res) => {
+	try {
+		console.log(req.params)
+		const details = req.params.details.split("&");
+		const phone = details[0];
+		const hash = details[1];
+		const otp = details[2];
 
-		const [oldHash, expiresIn]=hash.split(".");
-		if(expiresIn<Date.now()){
-			res.status(400).json({status:"failed", msg:"OTP Expired"});
+		const [oldHash, expiresIn] = hash.split(".");
+		if (expiresIn < Date.now()) {
+			res.status(400).json({ status: "failed", msg: "OTP Expired" });
 		}
-		const newHash=createHashedString(`${phone}.${otp}.${expiresIn}`);
-		if(newHash!=oldHash){
-			res.status(400).json({status:"failed", msg:"OTP Expired"});
+		const newHash = createHashedString(`${phone}.${otp}.${expiresIn}`);
+		if (newHash != oldHash) {
+			res.status(400).json({ status: "failed", msg: "OTP Expired" });
 		}
-		res.status(200).json({status:"success"});
-	}catch(e){
-		res.status(400).json({status:"error"});
+		res.status(200).json({ status: "success" });
+	} catch (e) {
+		res.status(400).json({ status: "error" });
 		console.log(e);
 	}
-	
+
 })
 
 
-const createHashedString=(data)=>{
-	const hashedStr=crypto.createHmac("sha256", "secret")
-	.update(data).digest("hex");
+const createHashedString = (data) => {
+	const hashedStr = crypto.createHmac("sha256", "secret")
+		.update(data).digest("hex");
 	return hashedStr;
 }
 
-const sendOtp=async (phone,otp)=>{
-    console.log(phone)
-	return await twilio.messages.create({
-		to:phone,
-		from:"+18165788289",
-		body:`You OTP for Lybley is ${otp}`
-	})
+const sendOtp = async (phone, otp) => {
+	const options = {
+		"method": "POST",
+		"hostname": "api.msg91.com",
+		"port": null,
+		"path": "/api/v5/flow/",
+		"headers": {
+			"authkey": process.env.AUTHTOKEN,
+			"content-type": "application/json"
+		}
+	};
+
+	const req = http.request(options, function (res) {
+		const chunks = [];
+
+		res.on("data", function (chunk) {
+		  chunks.push(chunk);
+		});
+	  
+		res.on("end", function () {
+		  const body = Buffer.concat(chunks);
+		  console.log(body.toString());
+		});
+	});
+	req.write(`\n{\n  \"flow_id\": \"${process.env.FLOWIDOTP}\",\n  \"recipients\": [\n    {\n      \"mobiles\": \"${phone}\",\n      \"var\": \"${otp}\"\n    }\n  ]\n}`);
+	req.end();
 }
 
-module.exports=router;
+module.exports = router;
