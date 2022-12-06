6c7a744618dd54ac27d4f896ac61525127c750dd
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2022-12-06 11:43:16 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -164,7 +164,7 @@ router.get("/all",async (req,res)=>{
 	}
 })
 
-router.post("/cart",async (req,res)=>{
+router.get("/cart",async (req,res)=>{
 	try{
 		console.log(req.body);
 		res.status(200).json("product");
