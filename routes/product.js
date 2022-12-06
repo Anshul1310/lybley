be38e90f1d4fe2d4648c7f56fc28f6f95f0aee05
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2022-12-06 11:41:12 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -166,7 +166,7 @@ router.get("/all",async (req,res)=>{
 
 router.post("/cart",async (req,res)=>{
 	try{
-		console.log(req.params);
+		console.log(req.body);
 		res.status(200).json("product");
 	}catch(er){
 		res.status(404).json({msg:"Something went wrong"})
